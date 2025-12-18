cea757a784e436b69c824ddc8b97d4f0c102e709
vboxuser <antoniwnek123@gmail.com>
2025-12-18 17:32:29 +0100

    fix

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -95,11 +95,10 @@ TEMPLATES = [
 # ========================
 
 DATABASES = {
- "default": dj_database_url.config(
- default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
- conn_max_age=600,
- conn_health_checks=True,
- )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -22,7 +22,6 @@ import hello.views
 
 urlpatterns = [
     path("", hello.views.index, name="index"),
-    path("db/", hello.views.db, name="db"),
     # Uncomment this and the entry in `INSTALLED_APPS` if you wish to use the Django admin feature:
     # https://docs.djangoproject.com/en/6.0/ref/contrib/admin/
     # path("admin/", admin.site.urls),
